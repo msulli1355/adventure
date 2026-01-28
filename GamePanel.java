c0582449b44d9a30694a94bfeeb2af1576285d9e
msulli1355 <45134364+msulli1355@users.noreply.github.com>
2026-01-28 13:20:31 -0600

    Add files via upload
    GamePanel inital commit

---
+++ GamePanel.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel 
+{
+	//SCREEN SETTINGS
+	final int originalTileSize = 16; //16x16 tile
+	final int scale = 3;
+	
+	final int tileSize = originalTileSize * scale;//48x48 tile
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol;//768px
+	final int screenHeight = tileSize * maxScreenRow;//576px
+
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		
+		
+	}
+}
