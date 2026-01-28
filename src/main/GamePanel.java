79419f7b3b7ac07950a57204a6fc9f0787beca99
Michael Sullivan <msulli1355@gmail.com>
2026-01-28 13:30:52 -0600

    Second commit - completed video #1

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel 
+public class GamePanel extends JPanel implements Runnable 
 {
 	//SCREEN SETTINGS
 	final int originalTileSize = 16; //16x16 tile
@@ -16,12 +16,24 @@ public class GamePanel extends JPanel
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol;//768px
 	final int screenHeight = tileSize * maxScreenRow;//576px
+	
+	Thread gameThread;
 
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
 		
+		
+	}
+
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
 	}
 }
