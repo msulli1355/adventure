2cdd27f6a7a38857eeb13f9f90235f11a0aaf6f6
Michael Sullivan <msulli1355@gmail.com>
2026-01-28 13:01:45 -0600

    First commit

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setTitle("2D Adventure");
+		window.setResizable(false);
+		GamePanel panel = new GamePanel();
+		window.add(panel);
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+	}
+}
+
